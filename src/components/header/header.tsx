44a1df4cb2a363df719083ad3897495c2df7a125
Sergey <redmoloch@yandex.ru>
2022-05-13 14:01:56 +0300

    added login modal

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,20 +1,32 @@
-import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Box, Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, TextField, Toolbar, Typography } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme: Theme) => ({
     menuButton: { margin: theme.spacing(1) },
     title: { flexGrow: 1 },
     appBar: { backgroundColor: '#240202' },
-    link: { color: 'white', textDecoration: 'none' }
-
+    link: { color: '#fff', textDecoration: 'none' },
+    modal: { color: '#fff', backgroundColor: '#000' },
+    modalContent: { color: '#fff', backgroundColor: '#240202' },
+    modalText: { color: '#fff' },
+    modalField: { backgroundColor: '#fff' },
 }))
 
 
 export const Header = () => {
 
     const classes = useStyles()
+
+    const [isLoginOpen, setLoginOpen] = useState(false);
+
+    const handleLoginClick = () => setLoginOpen(true);
+    const handleLoginCloseClick = () => setLoginOpen(false);
+
+
+
     return (
         <AppBar position="fixed" className={classes.appBar}>
             <Container fixed>
@@ -31,7 +43,36 @@ export const Header = () => {
                     </Typography>
 
                     <Box mr={2}>
-                        <Button color="inherit" variant="outlined">Log In</Button>
+                        <Button color="inherit" variant="outlined" onClick={handleLoginClick}>Log In</Button>
+                        <Dialog open={isLoginOpen} onClose={handleLoginCloseClick} aria-labelledby={'form-login-modal'} >
+                            <DialogTitle id="form-login-modal"  className={classes.modal}> Log In </DialogTitle>
+                            <DialogContent className={classes.modalContent}>
+                                <DialogContentText className={classes.modalText}> Enter Email and Password </DialogContentText>
+                                <TextField 
+                                    autoFocus 
+                                    margin="dense"
+                                    id="email"
+                                    type={'email'}
+                                    fullWidth
+                                    placeholder="Your Email"
+                                    className={classes.modalField}
+                                    
+                                />
+                                <TextField 
+                                    margin="dense"
+                                    id="password"
+                                    type={'password'}
+                                    fullWidth
+                                    placeholder="Your Password"
+                                    className={classes.modalField}
+
+                                />
+                            </DialogContent>
+                            <DialogActions  className={classes.modal}>
+                                <Button onClick={handleLoginCloseClick} color="secondary" variant="outlined">Cancel</Button>
+                                <Button onClick={handleLoginCloseClick} color="primary" variant="outlined">Ok</Button>
+                            </DialogActions>
+                        </Dialog>
                     </Box>
                     <Button color="secondary" variant="contained">Sign Up</Button>
                 </Toolbar>
